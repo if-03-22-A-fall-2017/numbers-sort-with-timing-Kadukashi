78191fae5ad3dabb8b8ae1a027e33de59d9ed910
Gabriel <31956986+Kadukashi@users.noreply.github.com>
2018-03-01 12:19:12 +0100

    Revert "Sorry for the late upload."

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -1,45 +1,12 @@
 /*-----------------------------------------------------------------------------
- *				HTBLA-Leonding / Class: 2AHIF
+ *				HTBLA-Leonding / Class: <your class name here>
  *-----------------------------------------------------------------------------
  * Exercise Number: #exercise_number#
  * File:			sorting_algorithms.cpp
- * Author(s):		Gabriel Aigner
+ * Author(s):		<your name>
  * Due Date:		#due#
  *-----------------------------------------------------------------------------
  * Description:
  * <your description here>
  *-----------------------------------------------------------------------------
 */
-#include "sorting_algorithms.h"
-
-void init_random(int* array,int size){
-  for (int i = 0; i < size; i++) {
-    array[i] = rand();
-   }
-}
-
-void bubble_sort(int* array,int size){
-  int temp = 0;
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      if(array[i] < array[j])
-      {
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-    }
-  }
-}
-
-void insertion_sort(int* array,int size){
-for (int i = 0; i < size; i++) {
-  int number = array[i];
-  int j = i-1;
-  while (j >= 0 && array[j] > number) {
-    array[j+1] = array[j];
-    j = j -1;
-  }
-  array[j+1] = number;
-}
-}
--- sorting_algorithms.h
+++ sorting_algorithms.h
@@ -10,13 +10,16 @@
  * <Add description>
  * ----------------------------------------------------------
  */
- #ifndef ___SORTING_ALGORITHMS_H
- #define ___SORTING_ALGORITHMS_H
- #include <stdio.h>
- #include <stdlib.h>
+#ifndef ___SORTING_ALGORITHMS_H
+#define ___SORTING_ALGORITHMS_H
 
- void init_random(int* array,int size);
- void bubble_sort(int* array,int size);
- void insertion_sort(int* array,int size);
+/**
+*** @file sorting_algorithms.h
+*** @brief Description of sorting algorithms
+*** @page Sorting Algorithms
+*** Basically two functions to sort arrays of integer elements. Furthermore, a function
+*** to initialize an array with a number of random elements.
+*/
 
- #endif
+
+#endif
